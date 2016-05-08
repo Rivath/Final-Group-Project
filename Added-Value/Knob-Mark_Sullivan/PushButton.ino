6ccea60871f918147160f666fd4a3eddaee87151
sulliv35 <sulliv35@miamioh.edu>
2016-05-08 14:53:23 -0400

    Added push button code

---
+++ Added-Value/Knob-Mark_Sullivan/PushButton.ino
@@ -0,0 +1,17 @@
+//Mark Sullivan
+int buttonIn;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(12,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonIn = digitalRead(12);
+  Serial.println(buttonIn);
+  if(buttonIn){//add check for sides completed
+    Serial.println("Button pressed");
+    //win occured
+  }
+}
